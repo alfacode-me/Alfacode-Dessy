e2954641abff77ca825b622de0a3e4c9b9d8c18b
alfacode.me <email@alfacode.me>
2016-12-14 13:18:48 +0700

    OK

--- config/app.js
+++ config/app.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "Dessy",
     fullname: "Decision Support System",
     versi: "1.0.23",
-    port: "2323",
+    port: "2381",
     database: "dessy",
     secret: "surya230496#dessy@alfacode.me",
     timezone: "Asia/Jakarta"
